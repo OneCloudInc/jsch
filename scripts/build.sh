798b2c534162ccfcb13143e3a7c3666e0b001d6c
Joe Waters <joe.waters@workiva.com>
2022-06-13 13:44:45 -0500

    STUD-935: Try including pom.xml in javadoc jar

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 BUILD_CMD="mvn package"
 
 if [ "${GIT_TAG}" ]
 then
   RELEASE_VERSION=${GIT_TAG}
 	echo "Detected tagged build version ${GIT_TAG}. Setting version to ${RELEASE_VERSION}"
-	BUILD_CMD+=" -DarchetypeVersion=${RELEASE_VERSION}"
+	mvn versions:set -DnewVersion=${GIT_TAG}
 else
 	echo "No tag detected on the current commit. This is a non-release build."
 fi
